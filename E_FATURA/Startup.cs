e66a6b49190ceb5fd3f3776e26a3d1dbd400238b
colog <mail.cihanbalci@gmail.com>
2017-10-04 16:35:31 +0300

    Add project files.

---
+++ E_FATURA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_FATURA.Startup))]
+namespace E_FATURA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
